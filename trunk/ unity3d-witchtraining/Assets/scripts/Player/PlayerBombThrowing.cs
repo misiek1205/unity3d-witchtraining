e7a6edfd6cb6fe802f1ae2e11d3d380e8d6f37d3
scottpetrovic <scottpetrovic@cbf8d32e-675b-3fa4-4c46-31bf22a3258f>
2010-10-23 02:06:20 +0000

    fixed bomb throwing animation and timing. Stopped bomb throwing power from getting too strong

--- trunk/ unity3d-witchtraining/Assets/scripts/Player/PlayerBombThrowing.cs
+++ trunk/ unity3d-witchtraining/Assets/scripts/Player/PlayerBombThrowing.cs
@@ -19,15 +19,19 @@ public class PlayerBombThrowing : MonoBehaviour
     private bool canThrowBomb = true;
     private float bombOffsetTime = 1.0f;
 
+    //temporary bomb object while holding it
+    private GameObject bombTemp;
+    private bool isHoldingBomb = false;
+
     // Use this for initialization
     void Awake()
     {
         _player = GetComponent<PlayerManager>();
 
-        animationGameObject.animation["throwWind"].layer = 9;
+        animationGameObject.animation["throwWind"].layer = 11;
         animationGameObject.animation["throwWind"].wrapMode = WrapMode.ClampForever;
 
-        animationGameObject.animation["throwRelease"].layer = 9;
+        animationGameObject.animation["throwRelease"].layer = 11;
         animationGameObject.animation["throwRelease"].wrapMode = WrapMode.Once;
 
         animationGameObject.animation["throwRelease"].AddMixingTransform(torso);
@@ -46,16 +50,28 @@ public class PlayerBombThrowing : MonoBehaviour
             bombPowerTimer = Time.time;
 
             animationGameObject.animation.Blend("throwWind", 1.0f, 0.2f);
-            GameObject bomb = (GameObject)Instantiate(bombPrefab, rightHand.transform.position, Quaternion.LookRotation(_player.MoveDirection()));
-            bomb.rigidbody.useGravity = false;
-            bomb.rigidbody.velocity = Vector3.zero;
-            bomb.rigidbody.position = rightHand.transform.position;
-            bomb.gameObject.transform.parent = rightHand.transform;
+            bombTemp = (GameObject)Instantiate(bombPrefab, transform.position, Quaternion.LookRotation(_player.MoveDirection()));
+
+            bombTemp.gameObject.transform.parent = rightHand.transform;
+            bombTemp.gameObject.transform.position = rightHand.transform.position;
+
+
+            //take off all gravity
+            bombTemp.gameObject.rigidbody.useGravity = false;
+            bombTemp.gameObject.rigidbody.velocity = Vector3.zero;
+            bombTemp.gameObject.rigidbody.rotation = Quaternion.identity;
+
+            //bool is for making sure the bomb's position doesn't veer when moving around
+            // will allow KeepBombPosition to function
+            isHoldingBomb = true;
 
         }
         
         if (Input.GetButtonUp("Bomb") && canThrowBomb)
         {
+            //delete bomb object that is being held
+            Destroy(bombTemp);
+            isHoldingBomb = false;
 
             animationGameObject.animation.Play("throwRelease");
 
@@ -66,7 +82,16 @@ public class PlayerBombThrowing : MonoBehaviour
 
             //instantiate and throw the bomb
             GameObject bomb = (GameObject)Instantiate(bombPrefab, rightHand.transform.position, Quaternion.LookRotation(_player.MoveDirection()));
-            bomb.rigidbody.velocity = _player.MoveDirection() * 50.0f * bombTimerLength;
+
+            //make sure the strongest it can get is only after 3 seconds and no more
+            if (bombTimerLength > 3)
+            {
+                bomb.rigidbody.velocity = _player.MoveDirection() * 50.0f * 3;
+            }
+            else
+            {
+                bomb.rigidbody.velocity = _player.MoveDirection() * 50.0f * bombTimerLength;
+            }
 
             canThrowBomb = false;
         }
@@ -77,7 +102,16 @@ public class PlayerBombThrowing : MonoBehaviour
             canThrowBomb = true;
         }
 
-
+        if (isHoldingBomb)
+        {
+            KeepBombPosition();
+        }
         
+    }
+
+
+    private void KeepBombPosition()
+    {
+        bombTemp.gameObject.transform.position = rightHand.transform.position;
     }
 }
