1da14dc27cf51512a40257bede43d19c65054b37
scottpetrovic <scottpetrovic@cbf8d32e-675b-3fa4-4c46-31bf22a3258f>
2010-10-25 04:31:00 +0000

    changed player movement and removed input wrapper scripts/SmoothInputAxes.cs. Movement is much more fluid now.

---  unity3d-witchtraining/Assets/scripts/Player/ThirdPersonFlyingController.cs
+++  unity3d-witchtraining/Assets/scripts/Player/ThirdPersonFlyingController.cs
@@ -8,17 +8,19 @@ public class ThirdPersonFlyingController : MonoBehaviour
 
 
     public float flyingSpeed = 15.0f;
+    public float flyingBackSpeed = -5.0f;
     
     private float runFlyMultiplier = 3.1f;
-    private float elevateMultipler = 4.0f;
+    public float elevateMultipler = 4.0f;
     
     private bool isFlying = false;
+    private bool isFlyingBackward = false;
     private ThirdPersonController characterController;
     // Move the controller
     CharacterController controller;
     private float jumpOffset = 0.5f;
     
-    //helps Lerp work right when doing movements
+    //helps Lerp work right when doing moveSpeeds
    // private float a = 0.0f;
 
 
@@ -28,14 +30,13 @@ public class ThirdPersonFlyingController : MonoBehaviour
     // The current move direction in x-z
     private Vector3 moveDirection = Vector3.zero;
     // The current x-z move speed
-   // private float currSpeed = 0.0f;
-    // The speed when walking
-    //public float walkSpeed = 3.0f;
+    private float moveSpeed = 0.0f;
     public float rotateSpeed = 100.0f;
     public float speedSmoothing = 10.0f;
-
+    private float elevateSpeed = 0.0f;
+    public float elevateAmount = 7.0f;
     //Class that smoothes input when pressed
-    private SmoothInputAxes axes;
+    //private SmoothInputAxes axes;
     
     //alternate camera when in fly mode
     //private PlayerCamera normalCameraSmoothFollow;
@@ -50,7 +51,7 @@ public class ThirdPersonFlyingController : MonoBehaviour
         characterController = GetComponent<ThirdPersonController>();
         controller = GetComponent<CharacterController>();
 
-        axes = GetComponent<SmoothInputAxes>();
+        //axes = GetComponent<SmoothInputAxes>();
 
 
         moveDirection = transform.TransformDirection(Vector3.forward);
@@ -101,21 +102,14 @@ public class ThirdPersonFlyingController : MonoBehaviour
     {
 
         // ----------------Movement Controls      ---------------------//
-      //  Transform cameraTransform = Camera.main.transform;
+      
 
 
-       //--- find out the direction of the movement
-       //first we don't want to rotate if pressing the down button, so make it equal to zero if pressed
-      /*  float axesV;
-        if (axes.GetV() < 0.0f)
-           axesV = 0.0f;
-        else
-           axesV = axes.GetV();
-        */
+
 
         //sets target direction by making a vector with the keys that you have pressed.
         // the 1.0f is the vertical value. If you try to turn with only h being pressed, it will act funny
-        Vector3 targetDirection = axes.GetH() * transform.right + 0.3f * transform.forward;
+        Vector3 targetDirection = Input.GetAxis("Horizontal") * transform.right + 0.3f * transform.forward;
        targetDirection = targetDirection.normalized; 
 
        //creates a vector that will smoothly rotate toward destination. Normalized for good measure    
@@ -127,69 +121,116 @@ public class ThirdPersonFlyingController : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(moveDirection);
 
 
-            
-
         //--Find out the movement
-        //We want to make sure the backward movement is a lot slower than forward movement
-        // If he v input input is less than .2 clamp it to that.
-        float axesVm;
-       
-        if (axes.GetV() < -0.3f)
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector3 targetDirection2 = h * transform.right + v * transform.forward;
+
+        // Choose target speed
+        //* We want to support analog input but make sure you cant walk faster diagonally than just forward or sideways
+        float targetSpeed = Mathf.Min(targetDirection2.magnitude, 1.0f);
+
+        if (v < 0.1f && v > -0.1f)
         {
-            axesVm = -0.3f;
+            targetSpeed = 0;
+            isFlyingBackward = false;
+        }
+        else if (v < -0.1f)
+        {
+            targetSpeed = flyingBackSpeed;
+            isFlyingBackward = true;
         }
         else
         {
-            axesVm = axes.GetV();
+            targetSpeed = flyingSpeed;
+            isFlyingBackward = false;
         }
+                   
+            
 
-        Vector3 targetDirection2 = axes.GetH() * transform.right + axesVm * transform.forward;
-        Vector3 moveAmount = targetDirection2 * flyingSpeed;
-        moveAmount *= Time.deltaTime;
+
 
         // Pick speed modifier if Run is pressed
         if (Input.GetButton("Run"))
         {
-            moveAmount *= runFlyMultiplier;
+            //only add multiplier if going forward
+            if (!isFlyingBackward)
+            {
+                targetSpeed *= runFlyMultiplier;
+            }
         }
 
-        collisionFlags = controller.Move(moveAmount);
-            
+
+
+        //sets the current smooth speed
+        // Smooth the speed based on the current target direction
+        float curSmooth = speedSmoothing * Time.deltaTime;
+
+
+        //sets the movement after all the scenarios have been calculated
+        moveSpeed = Mathf.Lerp(moveSpeed, targetSpeed, curSmooth);
+        //Debug.Log("Move Speed:" + moveSpeed + " Target Speed:" + targetSpeed + " CurSmoth:" + curSmooth );
+
+        // Calculate actual motion
+        Vector3 movement = moveDirection * moveSpeed;
+        movement *= Time.deltaTime;
+
+
+
+        collisionFlags = controller.Move(movement);
+
+           
 
 
 
         
             // ----------------Elevate Controls      ---------------------//
+        float elevateTargetSpeed = 0.0f;
+       // Vector3 riseAmount = new Vector3(0.0f, 7.0f * Time.deltaTime, 0.0f);
+       // Vector3 descendAmount = new Vector3(0.0f, -7.0f * Time.deltaTime, 0.0f);
 
-            if (Input.GetButton("Rise"))
+        Vector3 elevateDirection = Vector3.zero;
+
+        
+        if (Input.GetButton("Rise"))
             {
-                //transform.Translate(0.0f, 10.0f * Time.deltaTime, 0.0f);
-                Vector3 riseAmount = new Vector3(0.0f, 7.0f * Time.deltaTime, 0.0f);
 
-                if (Input.GetButton("Run"))
-                {
-                    riseAmount *= elevateMultipler;
-                }
-                
-                
-                collisionFlags = controller.Move(riseAmount);
-            }
-
-            if (Input.GetButton("Descend"))
-            {
-                //transform.Translate(0.0f, -10.0f * Time.deltaTime, 0.0f);
-                Vector3 descendAmount = new Vector3(0.0f, -7.0f * Time.deltaTime, 0.0f);
+                elevateDirection = Vector3.up;    
                 
                 if (Input.GetButton("Run"))
                 {
-                    descendAmount *= elevateMultipler;
+                    elevateTargetSpeed = elevateAmount * elevateMultipler;
                 }
-                
-                collisionFlags = controller.Move(descendAmount);
+                else
+                {
+                    elevateTargetSpeed = elevateAmount;
+                }         
+
             }
 
-          
 
+        if (Input.GetButton("Descend"))
+            {
+                elevateDirection = Vector3.down;
+
+                if (Input.GetButton("Run"))
+                {
+                    elevateTargetSpeed = elevateAmount * elevateMultipler;
+                }
+                else
+                {
+                    elevateTargetSpeed = elevateAmount;
+                }
+                
+            }
+
+
+        elevateSpeed = Mathf.Lerp(elevateSpeed, elevateTargetSpeed, curSmooth);
+        Vector3 elevateMovement = elevateSpeed * elevateDirection;
+        collisionFlags = controller.Move(elevateMovement);
+
+        Debug.Log(elevateDirection);
 
         }
 
