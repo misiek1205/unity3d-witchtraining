ef680beb0de16303fb7fdf8bc16b5b1e4ceb3436
scottpetrovic <scottpetrovic@cbf8d32e-675b-3fa4-4c46-31bf22a3258f>
2010-12-01 00:19:18 +0000

    added some miscellaneous unversioned files and added a LGPL license after some auditing. You know who you are :)

---
+++  unity3d-witchtraining/Assets/scripts/Player/PlayerAnimationEvents.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+	
+	
+	public AudioClip walkSound;
+	public AudioClip runSound;
+   
+	
+	private ThirdPersonPlayerAnimation playerAnimation;
+	private string movingState;
+	
+	void Start()
+	{
+	audio.loop = false;	
+	
+	playerAnimation = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonPlayerAnimation>();
+	
+	}
+	
+	
+	void Update()
+	{
+	//calculate which state the animation is in
+		movingState = playerAnimation.State();
+	
+	}
+	
+	
+public void walkAnimationEvent()
+	{
+		if (movingState == "Walk")
+		{
+		Debug.Log("Is walking");
+		audio.PlayOneShot(walkSound);
+		audio.volume = 0.4f;
+		}
+	}
+	
+	public void runAnimationEvent()
+	{
+		if (movingState == "Run")
+		{
+			Debug.Log("Is running");
+			audio.PlayOneShot(runSound);
+			audio.volume = 0.5f;
+		}
+	}
+}
